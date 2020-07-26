3dd1c96758cb1917165ec0d6c417c12be6036df6
Daniel <danielcandidosilwa@gmail.com>
2020-07-26 00:48:43 -0300

    chore: config routes search

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :power_generators, only: %i[index show] do
     get 'recommenda', on: :collection
     get 'freight_cost', on: :member
+    get 'search', on: :collection
   end
 end
